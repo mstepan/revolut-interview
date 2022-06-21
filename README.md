125cc8bb8f006dc8539e6f08cddad920b0f8029c
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-06-21 10:39:13 +0300

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # revolut-interview
 Temp repo for revolut interview
+=======
+# java 15 template project
+
+Use the following:
+* java 15 (set through jenv, check `.java-version` file)
+* maven wrapper with maven 3.6.3
+
+## Unit tests.
+
+```
+./mvnw clean test
+```
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * JVM parameter to print memory when JVM process exited:
+ * -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Maine done...");
+    }
+
+
+}

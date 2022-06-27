4c8dcc44faa106df78c33ebab8d5e7898dcd68f3
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-06-27 10:54:46 +0300

    equals & hashCode for Iterval added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,6 +38,30 @@ public class Main {
             return "[" + start + ", " + end + "]";
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Interval interval = (Interval) o;
+
+            if (start != interval.start) {
+                return false;
+            }
+            return end == interval.end;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = start;
+            result = 31 * result + end;
+            return result;
+        }
+
         public boolean hasIntersection(Interval cur) {
 
             Interval left = this;
